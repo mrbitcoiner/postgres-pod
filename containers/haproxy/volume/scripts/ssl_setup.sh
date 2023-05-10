e763cec93edca9c9b139a6c92f4dd34f2561ad35
mrbitcoiner <mrbitcoiner@tutanota.com>
2023-05-10 00:20:03 +0000

    * ssl_setup.sh   - Change execution order

--- containers/haproxy/volume/scripts/ssl_setup.sh
+++ containers/haproxy/volume/scripts/ssl_setup.sh
@@ -5,12 +5,11 @@ set -e
 readonly ADMINER_CERT_DIR="/app/data/certs/adminer"
 ####################
 
-/app/scripts/ssl_config.sh adminer
+su -c '/app/scripts/ssl_config.sh adminer' ${CONTAINER_USER}
 
 if ! [ -e "${ADMINER_CERT_DIR}/server.pem" ]; then
   cat ${ADMINER_CERT_DIR}/server.crt ${ADMINER_CERT_DIR}/server.key > ${ADMINER_CERT_DIR}/server.pem 
 fi
 
-su -c '/app/scripts/ssl_config.sh adminer' ${CONTAINER_USER}
 
 
